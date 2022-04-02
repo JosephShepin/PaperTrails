f6b90bfb4bfad49828fbf5ec97ec90290c0332ca
Joseph Shepin <jshepin@gmail.com>
2022-04-02 14:50:24 -0500

    added gitignore, readme

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -3,9 +3,9 @@
     <div class="container shadow p-3 mb-5 bg-white rounded">
       <h1>Welcome to Lorem Ipsum</h1>
       <br>
-      <input class="form-control address-input" placeholder="Enter Address">
+      <input class="form-control address-input" placeholder="Enter Candidate Name">
       <br>
-      <button class="btn btn-primary">Continue</button>
+      <button class="btn btn-primary">Search</button>
     </div>
   </div>
 </template>
