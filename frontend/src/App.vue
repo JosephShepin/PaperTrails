982154ac7e4ef4018139ab37a32cba19a305e32f
Joseph Shepin <jshepin@gmail.com>
2022-04-03 10:55:07 -0500

    added gh link

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,6 +8,9 @@
             <li class="nav-item">
               <a class="nav-link" href="/" style="padding-right: 25px">Home</a>
             </li>
+            <li class="nav-item">
+              <a class="nav-link" href="https://github.com/JosephShepin/PaperTrails" style="padding-right: 25px">Github</a>
+            </li>
           </ul>
         </div>
       </nav>
